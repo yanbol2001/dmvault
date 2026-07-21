f028f7e3589e7abb9d24dc8bc0b40af4414b17ad
yanbol2001 <yanbol2001@gmail.com>
2026-07-21 13:48:13 +0800

    V7_5_0_Godzilla

--- core/feedback-config.js
+++ core/feedback-config.js
@@ -1,8 +1,7 @@
 window.DMVAULT_FEEDBACK_CONFIG = Object.freeze({
-  // Google Apps Script 網頁應用程式 /exec 網址
-  endpoint: "https://script.google.com/macros/s/AKfycbw6CspvKB0Cn1VGymuLL_7NBIu8ApuEgU6DF4o3ioR-2GawYQ1S8zlqzqE6IGPs6GBH9A/exec",
+  // 部署 Google Apps Script 網頁應用程式後，把網址貼在引號內。
+  endpoint: '',
   enabled: true,
-  minMessageLength: 5,
   maxMessageLength: 2000,
-  maxQueueSize: 100
+  cooldownSeconds: 15
 });
