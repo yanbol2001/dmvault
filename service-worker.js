135db4fa11368f39aa1099049d96092990975678
yanbol2001 <yanbol2001@gmail.com>
2026-07-21 12:20:07 +0800

    v1_4_1_Floating

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "dmvault-platform-1.2.4-feedback-context-v1";
+const VERSION = "dmvault-platform-1.2.5-feedback-float-stack-v1";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const OFFLINE_CACHE_PREFIX = "dmvault-offline-package-";
