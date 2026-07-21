e614c4a3266dd4b6ccc665936ed0085fc687387e
yanbol2001 <yanbol2001@gmail.com>
2026-07-21 10:47:40 +0800

    v1_2_TopBanner

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "dmvault-platform-1.2.2-mh-mobile-source-static-v1";
+const VERSION = "dmvault-platform-1.2.3-feedback-banner-v1";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const OFFLINE_CACHE_PREFIX = "dmvault-offline-package-";
