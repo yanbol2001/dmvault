bb74a8d47f3759783e9aef8fdc68e7331d32af77
yanbol2001 <yanbol2001@gmail.com>
2026-07-21 11:23:29 +0800

    V1.3.1

--- core/feedback-config.js
+++ core/feedback-config.js
@@ -1,6 +1,6 @@
-window.DMVAULT_FEEDBACK_CONFIG = Object.freeze({
+window.DMVAULT_FEEDBACK_CONFIG = Object.freeze({ 
   // 部署 Google Apps Script 網頁應用程式後，把 /exec 網址貼在引號內。
-  endpoint: '',
+  endpoint: "https://script.google.com/macros/s/AKfycbw6CspvKB0Cn1VGymuLL_7NBIu8ApuEgU6DF4o3ioR-2GawYQ1S8zlqzqE6IGPs6GBH9A/exec",
   enabled: true,
   minMessageLength: 5,
   maxMessageLength: 2000,
