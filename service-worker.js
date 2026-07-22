3a8c7a3b5d757c861aeb43837f7017bc5be2dff6
yanbol2001 <yanbol2001@gmail.com>
2026-07-22 19:42:31 +0800

    V7.4.4.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "dmvault-platform-1.2.7-mh-uniform-target-cards";
+const VERSION = "dmvault-platform-1.2.8-mh-card-timer-layout-fix";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const OFFLINE_CACHE_PREFIX = "dmvault-offline-package-";
