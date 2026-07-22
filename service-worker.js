5fedb29e4e44385a37782098474587be93729184
yanbol2001 <yanbol2001@gmail.com>
2026-07-22 10:46:50 +0800

    Timer_Restore

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "dmvault-platform-1.2.5-godzilla-name-attack-fix";
+const VERSION = "dmvault-platform-1.2.6-godzilla-timer-restore";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const OFFLINE_CACHE_PREFIX = "dmvault-offline-package-";
